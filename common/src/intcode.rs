7abb5efab2b9824e2e7de628a44f512b15dbced2
mbilling <mbillingr@users.noreply.github.com>
2019-12-05 10:57:24 +0100

    add i/o support

--- common/src/intcode.rs
+++ common/src/intcode.rs
@@ -1,66 +1,181 @@
-pub struct Computer {
+
+pub type Computer = IoComputer<NoStream, NoStream>;
+
+pub struct IoComputer<I: Input, O: Output> {
     pub sr: Vec<i64>,
     pc: usize,
+    input: I,
+    output: O,
 }
 
-impl Computer {
+pub trait Input {
+    fn init() -> Self;
+    fn read(&mut self) -> i64;
+}
+
+pub trait Output {
+    fn init() -> Self;
+    fn write(&mut self, val: i64);
+}
+
+pub struct NoStream;
+
+impl Input for NoStream {
+    fn init() -> Self {NoStream}
+    fn read(&mut self) -> i64 {panic!("read from non-input")}
+}
+
+impl Output for NoStream {
+    fn init() -> NoStream {NoStream}
+    fn write(&mut self, _: i64) {panic!("write to non-output")}
+}
+
+impl<T: Iterator<Item=i64>> Input for T {
+    fn init() -> T {unimplemented!()}
+    fn read(&mut self) -> i64 {self.next().expect("Input underflow")}
+}
+
+impl Output for Vec<i64> {
+    fn init() -> Self {vec![]}
+    fn write(&mut self, val: i64) {self.push(val)}
+}
+
+impl<I: Input, O: Output> IoComputer<I, O> {
     pub fn new(input: &[i64]) -> Self {
-        Computer {
+        IoComputer {
             sr: input.to_vec(),
             pc: 0,
+            input: Input::init(),
+            output: Output::init(),
+        }
+    }
+
+    pub fn with_io(program: &[i64], input: I, output: O) -> Self {
+        IoComputer {
+            sr: program.to_vec(),
+            pc: 0,
+            input,
+            output,
         }
     }
 
     pub fn step(&mut self) -> Option<bool> {
-        match self.fetch() {
+        match self.fetch()? {
             Op::Halt => return Some(false),
-            Op::Add(a, b, c) => *self.sr.get_mut(c)? = self.sr.get(a)? + self.sr.get(b)?,
-            Op::Mul(a, b, c) => *self.sr.get_mut(c)? = self.sr.get(a)? * self.sr.get(b)?,
+            Op::Add(a, b, c) => self.set(c, self.get(a)? + self.get(b)?)?,
+            Op::Mul(a, b, c) => self.set(c, self.get(a)? * self.get(b)?)?,
+            Op::Inp(a) => {
+                let x = self.input.read();
+                self.set(a, x)?;
+            },
+            Op::Out(a) => self.output.write(self.get(a)?),
         }
-        return Some(true);
+        Some(true)
     }
 
     pub fn classify_step(&mut self, classification: &mut Vec<CellUse>) -> Option<bool> {
-        match self.peek().0 {
+        match self.peek()?.0 {
             Op::Halt => classification[self.pc].set_op(),
             Op::Add(a, b, c) | Op::Mul(a, b, c) => {
                 classification[self.pc].set_op();
-                classification[self.pc + 1].set_param();
-                classification[self.pc + 2].set_param();
-                classification[self.pc + 3].set_param();
-                classification[a].set_read();
-                classification[b].set_read();
-                classification[c].set_write();
+                self.classify_operand('R', self.pc+1, a, classification);
+                self.classify_operand('R', self.pc+2, b, classification);
+                self.classify_operand('W', self.pc+3, c, classification);
+            }
+            Op::Inp(a) => {
+                classification[self.pc].set_op();
+                self.classify_operand('W', self.pc+1, a, classification);
+            }
+            Op::Out(a) => {
+                classification[self.pc].set_op();
+                self.classify_operand('R', self.pc+1, a, classification);
             }
         }
         self.step()
     }
 
-    fn fetch(&mut self) -> Op {
-        let (op, delta) = self.peek();
-        self.pc += delta;
-        op
+    fn classify_operand(&mut self, mode: char, idx: usize, o: Operand, classification: &mut Vec<CellUse>) {
+        match o {
+            Operand::Imm(_) => {
+                classification[idx].set_immediate();
+            }
+            Operand::Pos(p) => {
+                classification[idx].set_param();
+                match mode {
+                    'R' => classification[p].set_read(),
+                    'W' => classification[p].set_write(),
+                    _ => panic!("invalid mode")
+                }
+            }
+        }
     }
 
-    pub fn peek(&self) -> (Op, usize) {
+    fn fetch(&mut self) -> Option<Op> {
+        let (op, delta) = self.peek()?;
+        self.pc += delta;
+        Some(op)
+    }
+
+    pub fn peek(&self) -> Option<(Op, usize)> {
         let i = self.pc;
-        let o = self.sr[i];
-        let a = self.sr.get(i + 1).map(|&x| x as usize).unwrap_or(99999);
-        let b = self.sr.get(i + 2).map(|&x| x as usize).unwrap_or(99999);
-        let c = self.sr.get(i + 3).map(|&x| x as usize).unwrap_or(99999);
-        match o {
+        let o = self.sr[i] % 100;
+        let fa = (self.sr[i] / 10000) % 10;
+        let fb = (self.sr[i] / 1000) % 10;
+        let fc = (self.sr[i] / 100) % 10;
+        let a = self.sr.get(i + 1).copied().unwrap_or(999999);
+        let b = self.sr.get(i + 2).copied().unwrap_or(999999);
+        let c = self.sr.get(i + 3).copied().unwrap_or(999999);
+        let a = Operand::new(fa, a);
+        let b = Operand::new(fb, b);
+        let c = Operand::new(fc, c);
+        Some(match o {
             1 => (Op::Add(a, b, c), 4),
             2 => (Op::Mul(a, b, c), 4),
+            3 => (Op::Inp(a), 2),
+            4 => (Op::Out(a), 2),
             99 => (Op::Halt, 1),
             _ => panic!("Unknown opcode: {}", o),
+        })
+    }
+
+    fn get(&self, o: Operand) -> Option<i64> {
+        match o {
+            Operand::Imm(i) => Some(i),
+            Operand::Pos(p) => self.sr.get(p).copied(),
+        }
+    }
+
+    fn set(&mut self, o: Operand, val: i64) -> Option<()> {
+        match o {
+            Operand::Imm(i) => None,
+            Operand::Pos(p) => self.sr.get_mut(p).map(|cell| *cell = val),
         }
     }
 }
 
+#[derive(Debug)]
 pub enum Op {
-    Add(usize, usize, usize),
-    Mul(usize, usize, usize),
+    Add(Operand, Operand, Operand),
+    Mul(Operand, Operand, Operand),
+    Inp(Operand),
+    Out(Operand),
     Halt,
+}
+
+#[derive(Debug)]
+pub enum Operand {
+    Pos(usize),
+    Imm(i64),
+}
+
+impl Operand {
+    pub fn new(flag: i64, x: i64) -> Self {
+        match flag {
+            0 => Operand::Pos(x as usize),
+            1 => Operand::Imm(x),
+            _ => panic!("Invalid flag")
+        }
+    }
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
@@ -69,6 +184,7 @@ pub struct CellUse {
     param: bool,
     write: bool,
     read: bool,
+    immediate: bool,
 }
 
 impl Default for CellUse {
@@ -78,6 +194,7 @@ impl Default for CellUse {
             param: false,
             write: false,
             read: false,
+            immediate: false,
         }
     }
 }
@@ -95,12 +212,21 @@ impl CellUse {
     pub fn set_read(&mut self) {
         self.read = true;
     }
+    pub fn set_immediate(&mut self) {
+        self.immediate = true;
+    }
 }
 
 impl std::fmt::Display for CellUse {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "{}", if self.op { "X" } else { "-" })?;
-        write!(f, "{}", if self.param { "P" } else { "-" })?;
+        write!(f, "{}", match (self.param, self.immediate) {
+            (true, true) => panic!("invalid flags"),
+            (true, false) => "P",
+            (false, true) => "I",
+            (false, false) => "-",
+        })?;
+        write!(f, "{}", if !self.param && self.immediate { "I" } else { "-" })?;
         write!(f, "{}", if self.read { "R" } else { "-" })?;
         write!(f, "{}", if self.write { "W" } else { "-" })
     }
@@ -146,5 +272,27 @@ mod tests {
         let mut c = Computer::new(&[1, 1, 1, 4, 99, 5, 6, 0, 99]);
         while c.step().unwrap() {}
         assert_eq!(c.sr, vec![30, 1, 1, 4, 2, 5, 6, 0, 99]);
+    }
+
+    #[test]
+    fn example5_1() {
+        let mut c = Computer::new(&[1002,4,3,4,33]);
+        while c.step().unwrap() {}
+        assert_eq!(c.sr, vec![1002,4,3,4,99]);
+    }
+
+    #[test]
+    fn example5_input() {
+        let mut c: IoComputer<_, _> = IoComputer::with_io(&[3, 3, 99, 0], std::iter::once(42), NoStream);
+        while c.step().unwrap() {}
+        assert_eq!(c.sr, vec![3, 3, 99, 42]);
+    }
+
+    #[test]
+    fn example5_output() {
+        let mut c: IoComputer<_, _> = IoComputer::with_io(&[4, 3, 99, 42], NoStream, vec![]);
+        while c.step().unwrap() {}
+        assert_eq!(c.sr, vec![4, 3, 99, 42]);
+        assert_eq!(c.output, vec![42]);
     }
 }
